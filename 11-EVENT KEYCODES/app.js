50a8c70bdd8d8d05cc51c83e053f910ca779620a
mesenve <97444737+mesenve@users.noreply.github.com>
2022-04-29 17:08:58 +0300

    Add files via upload

---
+++ 11-EVENT KEYCODES/app.js
@@ -0,0 +1,29 @@
+const insert = document.getElementById("insert");
+
+
+
+window.addEventListener("keydown" , (event) =>{
+
+    insert.innerHTML = `
+   
+         <div class="key">
+            ${event.key === " " ? "Space" : event.key }
+            <small>event.key</small>
+        </div>
+       
+        <div class="key">
+            ${event.keyCode}
+            <small>event.keyCoded</small>
+        </div>
+
+        <div class="key">
+           ${event.code}
+            <small>event.code</small>
+        </div>
+    
+    
+    
+    
+    `
+
+})
